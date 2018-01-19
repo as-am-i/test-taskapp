a2e7d85ed2a530385b9a186c4977fb198ccb59f8
asamitanii <g033.9521@gmail.com>
2018-01-19 11:57:43 -0800

    created users controller and index, show, new, and create action

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def index
+  	@users = User.all
+  end
+
+  def show
+  	@user = User.find_by(id: params[:id])
+  end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(name: params[:name], email: params[:email])
+  	if @user.save
+  		flash[:notice] = "Welcome, #{@user.name}. You have signed up successfully!"
+  		redirect_to("/users/#{@user.id}")
+  	else
+  		render("/users/new")
+  	end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   
-  get '/about' => 'welcome#about'
-  resources :tasks
+  get 'users/index'
+
+  get 'about' => 'welcome#about'
+  resources :tasks, :users
+  get 'signup' => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
