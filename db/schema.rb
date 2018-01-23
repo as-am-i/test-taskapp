738b68b31ef8b374d3e96546e429beaa55376f09
asamitanii <g033.9521@gmail.com>
2018-01-23 00:09:20 -0800

    added default user image and enabled users to change the user images in edit page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180119103341) do
+ActiveRecord::Schema.define(version: 20180121004454) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20180119103341) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image_name"
   end
 
 end
